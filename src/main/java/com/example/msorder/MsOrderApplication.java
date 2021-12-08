082d5641007492d37a657050b0d9a3854c7c5d0b
muskaplan <muskaplan@innova.com.tr>
2021-12-08 17:22:47 +0300

    import ErrorConfig from ms-common

--- src/main/java/com/example/msorder/MsOrderApplication.java
+++ src/main/java/com/example/msorder/MsOrderApplication.java
@@ -1,11 +1,19 @@
 package com.example.msorder;
 
+import com.example.mscommon.error.ErrorConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Import;
+
 
 @SpringBootApplication
+//@SpringBootApplication(scanBasePackages = {
+//                                            "com.training.spring",
+//                                            "com.ms.common.error"
+//})
 @ServletComponentScan
+@Import(ErrorConfig.class)
 public class MsOrderApplication {
 
     public static void main(String[] args) {
